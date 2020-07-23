128ac688dd2aca1391a165b74aa2f5a7fcdf71df
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-07-23 13:26:53 +0300

    Update cs43l22.c

--- cs43l22/cs43l22.c
+++ cs43l22/cs43l22.c
@@ -7,12 +7,12 @@
 	
 #include "cs43l22.h"
 
-I2C_HandleTypeDef 	CS43L22_i2c;			///<Handle I2C для работы с CS43L22
-I2S_HandleTypeDef	CS43L22_i2s;			///<Handle I2S для работы с CS43L22
+I2C_HandleTypeDef 	CS43L22_i2c;		///<Handle I2C для работы с CS43L22
+I2S_HandleTypeDef	CS43L22_i2s;		///<Handle I2S для работы с CS43L22
 GPIO_TypeDef		*CS43L22_reset_port;	///<Указатель на порт ножки Reset
-uint16_t 			LIS3DSH_reset_pin;		///<Ножка Reset
-CS43L22_ERROR_t 	CS43L22_code_error;		///<Ошибка
-chip 				CS43L22;				///<Уникальный номер  
+uint16_t 		LIS3DSH_reset_pin;	///<Ножка Reset
+CS43L22_ERROR_t 	CS43L22_code_error;	///<Ошибка
+chip 			CS43L22;		///<Уникальный номер  
 
 void 		CS43L22_error_handler(CS43L22_ERROR_t CS43L22_error);
 uint8_t 	CS43L22_i2c_read(CS43L22_REG_t read_address);
@@ -52,7 +52,7 @@ void CS43L22_mute(bool state) {
 /**
 	* @brief 	Инициализация CS43L22
 	* @details	Инициализация модуля I2C, модуля I2S, ножки Reset, установка кода отсутствия ошибки, выставление Reset в 1, чтение ID, рекомендуемые настройки из datasheet'a, 
-	*			инициализация ЦАП следующими параметрами:
+	*		инициализация ЦАП следующими параметрами:
 	* \details	Выключить питание
 	* \details	-Режим аудиосигнала
 	* \details 	--Отключение микрофона и включение наушников
@@ -64,7 +64,7 @@ void CS43L22_mute(bool state) {
 	* \details 	--Выключить ограничитель
 	* \details 	--Бас на -10,5 дБ
 	* \details 	--Установить уровень громкости +12 дБ по обоим каналам
-	* \details  -Режим аналогового сигнала
+	* \details	-Режим аналогового сигнала
 	* \details 	--I2S до 24 бит данных
 	* \details	Включить питание
 	* @param	Handle I2C, handle I2S, указатель на порт ножки Reset, ножка Reset, назначения ЦАП
@@ -84,17 +84,17 @@ void CS43L22_init(I2C_HandleTypeDef CS43L22_i2c_for_init, I2S_HandleTypeDef CS43
 				
 	CS43L22_i2c_write(CS43L22_REG_ADDR_PWR_CNTRL_1,	CS43L22_REG_PWR_CNTRL_1_WR_POWERED_DOWN);
 	if (purpose == CS43L22_AUDIO_SIGNAL) {
-		CS43L22_i2c_write(CS43L22_REG_ADDR_PWR_CNTRL_2, 			CS43L22_REG_PWR_CNTRL_2_WR_HEADPHONE_A_B_ON | CS43L22_REG_PWR_CNTRL_2_WR_SPEAKER_A_B_OFF);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_CLK_CNTRL, 				CS43L22_REG_CLK_CNTRL_WR_AUTO_DETECT_SPEED | CS43L22_REG_CLK_CNTRL_WR_MCLK_DIVIDED_BY_2);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_IFACE_CNTRL_1, 			CS43L22_REG_IFACE_CNTRL_1_WR_DAC_IFACE_FORMAT_I2S_UP_24B);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_PWR_CNTRL_2, 		CS43L22_REG_PWR_CNTRL_2_WR_HEADPHONE_A_B_ON | CS43L22_REG_PWR_CNTRL_2_WR_SPEAKER_A_B_OFF);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_CLK_CNTRL, 			CS43L22_REG_CLK_CNTRL_WR_AUTO_DETECT_SPEED | CS43L22_REG_CLK_CNTRL_WR_MCLK_DIVIDED_BY_2);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_IFACE_CNTRL_1, 		CS43L22_REG_IFACE_CNTRL_1_WR_DAC_IFACE_FORMAT_I2S_UP_24B);
 		CS43L22_i2c_write(CS43L22_REG_ADDR_MASTER_VOLUME_CNTRL_A,	CS43L22_REG_MASTER_VOLUME_CNTRL_WR_PLUS_12DB);
 		CS43L22_i2c_write(CS43L22_REG_ADDR_MASTER_VOLUME_CNTRL_B,	CS43L22_REG_MASTER_VOLUME_CNTRL_WR_PLUS_12DB);	
 		CS43L22_i2c_write(CS43L22_REG_ADDR_ANALOG_ZC_SR_SETTINGS,	CS43L22_REG_ANALOG_ZC_SR_SETTINGS_WR_NOT_OCCUR_SOFT_RAMP | CS43L22_REG_ANALOG_ZC_SR_SETTINGS_WR_NOT_OCCUR_ZERO_CROSS);
 		CS43L22_i2c_write(CS43L22_REG_ADDR_MISCELLANEOUS_CNTRLS,	CS43L22_REG_MISCELLANEOUS_CNTRLS_WR_DEEMPHASIS_EN);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_LIMITER_CNTRL_1,			CS43L22_REG_LIMITER_CNTRL_1_LIMITER_OFF);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_TONE_CNTRL,				CS43L22_REG_TONE_CNTRL_BASS_GAIN_MINUS_10_5DB);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_VOLUME_PCMA,				CS43L22_REG_VOLUME_PCM_PLUS_12DB);
-		CS43L22_i2c_write(CS43L22_REG_ADDR_VOLUME_PCMB,				CS43L22_REG_VOLUME_PCM_PLUS_12DB);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_LIMITER_CNTRL_1,		CS43L22_REG_LIMITER_CNTRL_1_LIMITER_OFF);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_TONE_CNTRL,			CS43L22_REG_TONE_CNTRL_BASS_GAIN_MINUS_10_5DB);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_VOLUME_PCMA,			CS43L22_REG_VOLUME_PCM_PLUS_12DB);
+		CS43L22_i2c_write(CS43L22_REG_ADDR_VOLUME_PCMB,			CS43L22_REG_VOLUME_PCM_PLUS_12DB);
 	} else {
 		CS43L22_i2c_write(CS43L22_REG_ADDR_IFACE_CNTRL_1, CS43L22_REG_IFACE_CNTRL_1_WR_DAC_IFACE_FORMAT_I2S_UP_24B);
 	}
